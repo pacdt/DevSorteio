cf6d08daad3f158d5fd9757439df0be2ee73c640
Mateus Santos <mateus15do2@gmail.com>
2023-10-14 21:30:01 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const firstNumber = document.querySelector("#first-number");
+const secondNumber = document.querySelector("#second-number");
+const winnerNumber = document.querySelector(".winner-number");
+
+const result = document.querySelector(".resultado")
+const form = document.querySelector(".formulario-sorteio")
+
+function sortear(){
+    let sortedNumber
+
+    const min = parseInt(firstNumber.value)
+    const max = parseInt(secondNumber.value)
+
+    sortedNumber = Math.floor(Math.random() * (max - min + 1) + min)
+
+    winnerNumber.innerHTML = sortedNumber
+
+    form.id = "show"
+    result.id = ""
+
+
+    console.log(sortedNumber)
+}
+
+function novoSorteio(){
+    form.id = ""
+    result.id = "show"
+}
